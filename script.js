a35e70047ba690472c4be4beb95a3998360786b5
NeginSal <neginsaljooghi@gmail.com>
2021-04-24 11:18:18 +0430

    message depeding on Time

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const button = document.querySelector("button");
+const output = document.querySelector(".output");
 button.addEventListener("click",showOutput);
 function showOutput(){
     const date = new Date();
-    console.log(date.getHours())
-    console.log("work")
+    let curr = date.getHours();
+    let message;
+    console.log(curr)
+
+    if(curr>17){
+        message="it's evening";
+    }else if(curr>12){
+        message="it's afternoon";
+    }else if (curr>0){
+        message="it's morning";
+    }else{
+        message= "something is wrong";
+    }
+
+    output.innerHTML="<h1>"+message+"</h1>";
 }
