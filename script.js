09b65946bcfb28aea7525425c7fcad048ba187bb
NeginSal <neginsaljooghi@gmail.com>
2021-04-27 11:20:30 +0430

    element background color update

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ function showOutput(){
     const date = new Date();
     let curr = date.getHours();
     let message;
-    console.log(curr)
+    console.log(curr);
 
     if(curr>17){
         message="it's evening";
+        output.style.backgroundColor="green";
     }else if(curr>12){
         message="it's afternoon";
+        output.style.backgroundColor="blue";
     }else if (curr>0){
         message="it's morning";
+        output.style.backgroundColor="orange";
     }else{
         message= "something is wrong";
+        output.style.backgroundColor="red";
     }
 
     output.innerHTML="<h1>"+message+"</h1>";
